a3aae1ece0eeb86a75598651a94a6f5a159ed82d
airnesygroup <airnesy.info@gmail.com>
2025-02-20 13:33:40 +0300

    new   commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -14,16 +14,19 @@ const Hero = () => {
 
         <div className="h-full p-2 box-border">
           <button
-            className="mt-4 bg-black text-white py-2 md:py-3 text-extrabold text-lg md:text-2xl 
-              px-4 md:px-6 rounded-[40px] border border-1 border-white cursor-pointer 
+            className="mt-4 bg-black text-white py-2  text-extrabold text-md md:text-xl 
+              px-4 rounded-[40px] border border-1 border-white cursor-pointer 
               hover:bg-white hover:text-black"
           >
             Presidential actions
           </button>
 
           <div className="max-w-[700px] text-center mx-auto">
-            <h1 className="text-6xl md:text-[70px] mt-[40px] mb-[40px] md:mb-[70px] font-official">
+            <h1 className="text-6xl md:text-[70px] mt-[40px] mb-[40px] md:mb-[70px] font-light">
+              <span className=" font-official">
               THE UNITED STATES OF AFRICA
+
+              </span>
             </h1>
           </div>
         </div>
