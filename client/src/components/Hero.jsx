de7b851743124db68f1c52c8423dafbd6e4792ef
airnesygroup <airnesy.info@gmail.com>
2025-02-20 14:47:47 +0300

    new   commit

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,45 +5,33 @@ import { useQuery } from "@tanstack/react-query";
 import CategoriesScroll from "./CategoriesScroll";
 
 const Hero = () => {
-  return (
-    <div 
   
-    className="flex  mb-[20px] bg-[#0d132d] flex-col h-screen ">
-      <div 
-        className="relative  w-full 
-          text-white text-center animate-fadeIn flex flex-col items-center justify-center"
-      >
+
+  return (
+    <div className="flex flex-col h-screen bg-[#0d132d] text-white">
+      {/* Content container */}
+      <div className="flex flex-col items-center justify-center flex-grow text-center animate-fadeIn">
         <img src="/whitehouse.webp" className="h-[100px] mx-auto mb-8" />
 
-        <div className="h-full p-2 box-border">
-          <button
-            className="mt-4  text-white py-1  text-extrabold text-sm md:text-lg 
-              px-5 rounded-[40px] border border-1 border-white cursor-pointer 
-              hover:border-gray-700"
-          >
-            PRESIDENTIAL ACTIONS
-          </button>
+        <button
+          className="mt-4 text-white py-1 text-extrabold text-sm md:text-lg 
+            px-5 rounded-[40px] border border-white cursor-pointer 
+            hover:border-gray-700"
+        >
+          PRESIDENTIAL ACTIONS
+        </button>
 
-          <div className="max-w-[700px] text-center mx-auto">
-            <h1 className="text-5xl md:text-[70px] mt-[40px] pl-9 pr-9  mb-[40px] md:mb-[70px] font-thin">
-              <span className=" font-official">
-              THE UNITED STATES OF AFRICA
-
-              </span>
-            </h1>
-          </div>
-       
-
-
+        <div className="max-w-[700px] mx-auto mt-[40px] mb-[40px] md:mb-[70px]">
+          <h1 className="text-5xl md:text-[70px] font-thin">
+            <span className="font-official">THE UNITED STATES OF AFRICA</span>
+          </h1>
         </div>
+      </div>
 
-        <div className="max-w-[900px] mx-auto text-center relative h-screen">
-  <div className="absolute bottom-[40px] left-0 right-0">
-    <CategoriesScroll />
-  </div>
+      {/* Categories at 80vh */}
+      <div className="mt-auto h-[20vh] flex items-end justify-center max-w-[900px] o">
 
-
-          </div>
+        <CategoriesScroll />
       </div>
     </div>
   );
